38b2046249b08f29a4fae0fd2a0b240d90a217ac
minagawa <Masahiko.Minagawa@sony.com>
2018-04-14 22:55:37 +0900

    TopSetting.storyboardとTopSettingViewController.swiftを作成．
    起動画面から遷移する最初の画面をTopSettin画面にしてみた．
    [TopSetting.storyboard] で [Is Initial View Controller]に設定しても [Main.storyboard] の [Is Initial View Controller]はそのまま．ViewController毎にあるようだ
    （あくまでStoryboard内の最初の画面ということ）
    
    LaunchScreenから遷移してくる画面は，Info.plist の [Main Story Board File Name] で設定するようだ．

---
+++ CompassTest01/TopSettingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TopSettingViewController.swift
+//  CompassTest01
+//
+//  Created by Minagawa on 2018/04/14.
+//  Copyright © 2018年 Minagawa. All rights reserved.
+//
+
+import UIKit
+
+class TopSettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+}
